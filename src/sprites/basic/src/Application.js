e6877b7c5f8577bb83152ada327022a1bcbb291e
Billy Lamberta <b@lamberta.org>
2012-12-07 14:13:01 -0800

    Add sprite basic example.

---
+++ src/sprites/basic/src/Application.js
@@ -0,0 +1,49 @@
+//## Create a SpriteView
+//Given the following project layout for resources:
+//<pre>
+//project
+//.
+//├── manifest.json
+//├── sdk/ -> /path/to/basil/sdk
+//├── build/
+//├── resources/
+//│   └── images/
+//│        └── characters/
+//│             ├── carl-idle-01.png
+//│             ├── carl-idle-02.png
+//│             ├── carl-idle-03.png
+//│             ├── carl-idle-04.png
+//│             ├── carl-walk-01.png
+//│             ├── carl-walk-02.png
+//│             └── carl-walk-03.png
+//└── src/
+//    └── Application.js
+//</pre>
+
+import ui.SpriteView as SpriteView;
+
+exports = Class(GC.Application, function () {
+
+	this._settings = {
+		logsEnabled: window.DEV_MODE,
+		showFPS: window.DEV_MODE,
+		clearEachFrame: true,
+		alwaysRepaint: true,
+		preload: []
+	};
+
+	this.initUI = function () {
+		var sprite = new SpriteView({
+			superview: this.view,
+			x: 0,
+			y: 0,
+			width: 60,
+			height: 60,
+			url: "resources/images/characters/carl",
+			defaultAnimation: 'idle'
+		});
+
+		//Play the 'walk' animation once, then return to idle.
+		sprite.startAnimation('walk');
+	};
+});
