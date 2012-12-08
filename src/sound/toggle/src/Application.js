f48399803af8530976c18325ad44411dcf1f8bea
Billy Lamberta <b@lamberta.org>
2012-12-07 16:00:08 -0800

    Add sound example from api to addon-examples.

---
+++ src/sound/toggle/src/Application.js
@@ -0,0 +1,65 @@
+//## Toggle a Sound
+
+//Given the following audio files in your project directory:
+//<pre>
+//project
+//.
+//├── manifest.json
+//├── sdk/ -> /path/to/basil/sdk
+//├── build/
+//├── resources/
+//│   └── sounds/
+//│       ├── music/
+//│       │   └── levelmusic.mp3
+//│       └── effect/
+//│           └── boink.mp3
+//└── src/
+//    └── Application.js
+//</pre>
+
+import device;
+import ui.View as View;
+import Sound;
+
+exports = Class(GC.Application, function () {
+
+  this.initUI = function () {
+    this.sound = new Sound({
+      path: 'resources/sounds',
+      files: {
+        levelmusic: {
+          background: true,
+          loop: true
+        }
+      }
+    });
+  };
+
+  this.buildView = function () {
+    var play = new View({
+      superview: this,
+      x: device.width/2 - 125,
+      y: 100,
+      width: 100,
+      height: 100,
+      backgroundColor: '#00ff00'
+    });
+
+    play.on('InputSelect', bind(this, function () {
+      this.sound.play('levelmusic');
+    }));
+
+    var stop = new View({
+      superview: this,
+      x: device.width/2 + 25,
+      y: 100,
+      width: 100,
+      height: 100,
+      backgroundColor: '#ff0000'
+    });
+
+    stop.on('InputSelect', bind(this, function () {
+      this.sound.pause('levelmusic');
+    }));
+  };
+});
