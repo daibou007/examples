0f34f7d62deeedc4f49ade4ddc2f47aa74921ba6
Jared Petker <Jared@gameclosure.com>
2013-01-16 03:19:09 -0800

    new text view clock example

---
+++ src/text/textviews-clock/src/Application.js
@@ -0,0 +1,78 @@
+//# Text View Clock <a title="View raw file" href="https://raw.github.com/gameclosure/addon-examples/master/src/text/textviews-clock/src/Application.js"><img src="../../include/download_icon.png" class="icon"></a>
+//Here we will create a completely working clock using Text Views!
+//Different positioning and scaling options are presented in this example
+//as well as how to update text views dynamically after creation.
+
+import device;
+import ui.TextView as TextView;
+
+exports = Class(GC.Application, function () {
+	this.initUI = function () {
+		//Create the header bar.
+		var title = new TextView({
+			superview: this.view,
+			autoFontSize: true,
+			x: 0,
+			y: 20,
+			height: 40,
+			width: this.view.style.width,
+			size: 50,
+			zIndex: 1,
+			text: "Text View Clock",
+			color: '#888888',
+			backgroundColor: 'white',
+			outlineColor: 'black',
+			verticalPadding: 5,
+			horizontalPadding: 20
+		});
+
+		// Create a TextView to hold the date
+		var dateTextView = new TextView({
+			superview: this.view,
+			autoFontSize: true,
+			x: 0,
+			y: title.style.y + title.style.height + 40,
+			height: 100,
+			width: this.view.style.width,
+			size: 50,
+			zIndex: 1,
+			wrap: true,
+			text: "Date\n",
+			color: '#FF8888',
+			backgroundColor: '#DDDDDD',
+			outlineColor: 'black',
+			verticalPadding: 5,
+			horizontalPadding: 20
+		});
+
+		// Create a TextView to hold the time
+		var timeTextView = new TextView({
+			superview: this.view,
+			autoFontSize: true,
+			x: 0,
+			y: dateTextView.style.y + dateTextView.style.height + 40,
+			height: 120,
+			width: this.view.style.width,
+			size: 60,
+			zIndex: 1,
+			wrap: true,
+			text: "Time\n",
+			color: '#4488FF',
+			outlineColor: 'white',
+			verticalPadding: 5,
+			horizontalPadding: 20
+		});
+
+		// Check what the date / time it is every half second, and 
+		// adjust date and time appropriately within the text views.
+		// Make sure we don't miss a second!
+		setInterval(bind(this, function() {
+			var date = new Date();
+			dateTextView.setText("Date\n" + date.toLocaleDateString().replace(/\//g, ' / '));
+			timeTextView.setText("Time\n" + date.toLocaleTimeString());
+		}, 500));
+	}
+});
+
+//Place this in the `Application.js` file of your project and you should see something like the following screenshot.
+//<img src="./doc/screenshot1.png" alt="many textviews screenshot" class="screenshot">
